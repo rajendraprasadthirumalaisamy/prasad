f43c82771ac0d2e38231a039deea06867499ba4e
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2017-09-08 16:37:29 +0530

    Create 2int odd.c

---
+++ 2int odd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int a,m =20,n=40;
+
+    
+
+    printf("Print Odd Numbers in a given range m to n:\n");
+
+    for (a = m; a<= n; a++)
+
+        {
+
+               if (n % 2 == 1)
+
+                  printf ("%d ", a);
+
+         }
+
+              
+
+}
